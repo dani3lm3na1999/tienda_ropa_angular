426be502d6cc2e38471dc75c4c89f6e4a120f85c
Spunicher <marlonjon1234@gmail.com>
2023-11-24 04:21:34 -0300

    f

--- src/app/tienda/tienda/tienda.component.ts
+++ src/app/tienda/tienda/tienda.component.ts
@@ -346,7 +346,7 @@ export class TiendaComponent implements OnInit {
       next: (r) => { },
       error: (e) => {},
       complete: () => {
-        //  this.routeService.navigate(['/Menu'])
+         this.routeService.navigate(['/Menu'])
       },
     });
   }
