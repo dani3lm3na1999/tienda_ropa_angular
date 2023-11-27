82cf61e6e9108143bb7e2aa3835cb54bef995ad9
dani3lm3na1999 <100393140+dani3lm3na1999@users.noreply.github.com>
2023-11-26 22:47:43 -0600

    Cambio de enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: false,
-  url: 'http://localhost:9000/api', 
-  firebaseConfig: {
-    apiKey: "AIzaSyC0OzycH4oW2qjKxWnsXMS7z8_QHM9e3fs",
-    authDomain: "tiendaonlinestorage.firebaseapp.com",
-    projectId: "tiendaonlinestorag",
-    storageBucket: "tiendaonlinestorage.appspot.com",
-    messagingSenderId: "835329623858",
-    appId: "1:835329623858:web:608b68f0da4f8ae17d392b",
-    measurementId: "G-K31ZM83L8K"
-  }
+  url: 'https://tienda-mind-api.onrender.com/api'
 };
